d3c1fa8b833327e7ab682dc1de1c5b90e481f2e5
AnnaRuben <shaked12072014@gmail.com>
2026-07-21 03:12:34 +0300

    Home Work Conditional Statements

---
+++ Conditionals/HomeWorkConditionalStatements.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Conditionals
+//{
+//    internal class HomeWorkConditionalStatements
+//    {
+//    }
+//}
+
+
+Console.Write("What is your first name: ");
+string? firstName = Console.ReadLine();
+
+Console.Write("What is your last name: ");
+string? lastName = Console.ReadLine();
+
+if (firstName.ToLower() == "tim" &&
+    lastName.ToLower() == "corey")
+{
+    Console.WriteLine("Hello Professor Corey");
+}
+else
+{
+    Console.WriteLine("Hello Student.");
+}
--- Conditionals/SwitchStatements.cs
+++ Conditionals/SwitchStatements.cs
@@ -10,24 +10,26 @@
 //}
 
 
-string firstName = "Sue";
-int age = 189;
+//string firstName = "Sue";
+//int age = 189;
 
-switch (age)
-{
-	case >= 0 and < 18:
-        Console.WriteLine("You are a child.");
-		break;
-	case >= 18 and < 66:
-        Console.WriteLine("You should have a job.");
-		break;
-	case >= 66:
-        Console.WriteLine("Hopefully you are retired or retiring soon.");
-		break;
-	default:
-        Console.WriteLine("Age was not in an expected range.");
-		break;
-}
+//switch (age)
+//{
+//	case >= 0 and < 18:
+//        Console.WriteLine("You are a child.");
+//		break;
+//	case >= 18 and < 66:
+//        Console.WriteLine("You should have a job.");
+//		break;
+//	case >= 66:
+//        Console.WriteLine("Hopefully you are retired or retiring soon.");
+//		break;
+//	default:
+//        Console.WriteLine("Age was not in an expected range.");
+//		break;
+//}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 
 
@@ -44,6 +46,9 @@ switch (age)
 //        Console.WriteLine("I don't know you.");
 //        break;
 //}
+
+
+
 
 
 
@@ -499,16 +504,3 @@ switch (age)
 
 //so which one to use does depend on the situation, but one of the benefits here in programing's that we have options.   
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-
-
-
-
-
-
-
-
-
-
-
